72ab0c4125ed8f0c4a154ca5e431c52aaf8b4f83
devansh2611 <64409960+devansh2611@users.noreply.github.com>
2022-05-14 09:47:24 +0530

    Update bfs_dfs.py

--- bfs_dfs.py
+++ bfs_dfs.py
@@ -1,40 +1,64 @@
-from collections import deque
+import collections
+
+class Graph:
+    graph = {}
+    def __init__(self):
+        pass
+
+    def addVertices(self):
+        n = int(input("Enter number of vertices: "))
+        for i in range(n):
+            vname = int(input("Enter name of vertex: "))
+            adj = set(map(int,input("Enter space separated adjacent vertices: ").split()))
+            self.graph[vname] = adj
+
+    def dfs(self,start):
+        visited = set()
+        visited.add(start)
+        l = []
+        l.append(start)
+        while len(l)>0:
+            x = l.pop()
+            visited.add(x)
+            print(x, end=" ")
+            l += list(self.graph[x] - visited)
+            # print(l)
+        print()
 
 
-vert=int(input("Enter the number of vertices: "))
-graph={}
-for i in range(vert):
-    ver=input("Enter the name of vertex: ")
-    edges=set(map(int,input("Enter space separated adjacent vertices: ").split()))
-    graph[ver]=edges
-print(graph)
-def dfs(start):
-    visited=set()
-    visited.add(start)
-    l=[]
-    l.append(start)
-    while len(l) > 0:
-        x=l.pop()
-        visited.add(x)
-        print(x,end=" ")
-        l+=list(graph[x]-visited)
-    print()
+    def bfs(self,start):
+        visited = set()
+        q = collections.deque()
+        q.append(start)
+        visited.add(start)
 
-def bfs(start):
-    visited=set()
-    visited.add(start)
-    q=deque()
-    q.append(start)
-    while (len(q) > 0):
-        v=q.popleft()
-        # visited.add(v)
-        print(v,end=" ")
-        for neighbour in graph[v]:
-            if neighbour not in visited:
-                visited.add(neighbour)
-                q.append(neighbour)
-    print()
-
-start=input("Enter start vertex: ")
-bfs(start)
-dfs(start)
+        while q:
+            vertex = q.popleft()
+            print(vertex,end=" ")
+            for neighbour in self.graph[vertex]:
+                if neighbour not in visited:
+                    visited.add(neighbour)
+                    q.append(neighbour)
+        print()
+g = Graph()
+g.addVertices()
+while True:
+    print("****************MENU*****************")
+    print("1. DFS")
+    print("2. BFS")
+    print("3. Change Graph")
+    print("0. Exit")
+    print("*************************************")
+    choice = int(input("Enter your choice: "))
+    if choice == 0:
+        exit(0)
+    elif choice == 1:
+        s = int(input("Enter starting vertex: "))
+        g.dfs(s,None)
+    elif choice == 2:
+        s = int(input("Enter starting vertex: "))
+        g.bfs(s,None)
+    elif choice == 3:
+        g.addVertices()
+    else:
+        print("Enter valid input!")
